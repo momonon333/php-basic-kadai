fb6c7d79c1812b5b2adff814fc044b049622466c
momonon333 <kurono1234masaki@gmail.com>
2025-11-22 14:18:53 +0900

    Add files via upload

---
+++ kadai_007/php/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $material = ['name' => 'onoin', 'price' => 200, 'weight' => 160];
+
+       print_r($material);
+       ?>
+   </p>
+</body>
+
+</html>
